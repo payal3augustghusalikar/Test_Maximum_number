f1ed91de99d12ebfdfbf76e47ef9d2e84228a621
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-03 13:45:29 +0530

    Added : function to check maximum amongt 3 integer

--- src/main/java/com/findmaxusinggenrics/FindMax.java
+++ src/main/java/com/findmaxusinggenrics/FindMax.java
@@ -4,4 +4,22 @@ public class FindMax {
     public static void main(String[] args) {
         System.out.println("***** Welcome to arithmetic number computation ****");
     }
+
+    public Integer findMaxValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer max = firstNumber;
+        if (secondNumber.compareTo(max) > 0) {
+            max = secondNumber;
+        }
+        if (thirdNumber.compareTo(max) > 0) {
+            max = thirdNumber;
+        }
+        printMax(max);
+        return max;
+    }
+
+     void printMax(Integer max) {
+        System.out.println("Largest = " + max);
+    }
 }
+
+
