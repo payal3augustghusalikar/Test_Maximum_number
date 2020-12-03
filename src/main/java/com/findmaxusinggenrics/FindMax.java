0a3145f9fcc5e8975f0d55b19e373f5541f2667e
payalghusalikar <payal.ghusalikar9@gmail.com>
2020-12-03 20:31:35 +0530

    Deleted : commented code

--- src/main/java/com/findmaxusinggenrics/FindMax.java
+++ src/main/java/com/findmaxusinggenrics/FindMax.java
@@ -23,25 +23,3 @@ public class FindMax {
         System.out.println("Max Value = " + max);
     }
 }
-
-
-
-
-   /* public <E extends Comparable> E findMaxValue(E firstValue, E secondValue, E thirdValue){
-        E max = firstValue;
-        if (secondValue.compareTo(max) > 0) {
-            max = secondValue;
-        }
-        if (thirdValue.compareTo(max) > 0) {
-            max = thirdValue;
-        }
-        printMax(max);
-        return max;
-    }
-
-    public <E> void printMax(E max){
-        System.out.println("Max Value = "+max);
-    }*/
-
-
-
